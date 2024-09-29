597ab260294ce362d1715a3e26f21fc6af3c6bdd
cooper <1633016740@qq.com>
2024-09-29 17:24:04 +0800

    1111

--- config/plugin.ts
+++ config/plugin.ts
@@ -1,11 +1,11 @@
 import { EggPlugin } from 'egg';
 
 const plugin: EggPlugin = {
-    // 开启sequelize-typescript
-    sequelize : {
-        enable: true,
-        package: 'egg-sequelize-ts',
-    }
+  // 开启sequelize-typescript
+  sequelize: {
+    enable: true,
+    package: 'egg-sequelize-ts',
+  },
 };
 
 export default plugin;
