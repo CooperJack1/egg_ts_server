a87cc9a1edddd9620c16838052672a1b9d1b3f46
cooper <1633016740@qq.com>
2024-10-12 17:29:10 +0800

    13

--- app/controller/util.ts
+++ app/controller/util.ts
@@ -3,6 +3,7 @@ import svgCaptcha = require('svg-captcha');
 export default class HomeController extends Controller {
   public async imageCode() {
     const { ctx } = this;
+    // 1.生成验证码
     const captcha = svgCaptcha.create({
       size: 4,
       width: 160,
@@ -13,6 +14,12 @@ export default class HomeController extends Controller {
       color: true,
       background: '#eee',
     });
+    // 2.保存验证码
+    ctx.session.captcha = {
+      code: captcha.text,
+      expire: Date.now() + 60 * 1000, // 有效期1min
+    };
+    // 3.将验证码发送到客户端
     ctx.body = captcha.data;
   }
 }
