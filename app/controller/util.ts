7cbcc795aa9a9f49e57b49518d2abb776ba885e3
cooper <1633016740@qq.com>
2024-10-14 10:37:03 +0800

    任务14完成

--- app/controller/util.ts
+++ app/controller/util.ts
@@ -1,6 +1,22 @@
 import { Controller } from 'egg';
 import svgCaptcha = require('svg-captcha');
 export default class HomeController extends Controller {
+  public async verifyImageCode() {
+    const { ctx } = this;
+    // 1.取出服务端中保存的验证码和过期时间
+    const serverCaptcha = ctx.session.captcha;
+    const serverCode = serverCaptcha.code;
+    const serverExpire = serverCaptcha.expire;
+    // 2.获取客户端传过来的验证码
+    const { clientCode } = ctx.query;
+    if (Date.now() > serverExpire) {
+      ctx.body = '验证码已过期';
+    } else if (serverCode !== clientCode) {
+      ctx.body = '验证码不正确';
+    } else {
+      ctx.body = '验证通过';
+    }
+  }
   public async imageCode() {
     const { ctx } = this;
     // 1.生成验证码
