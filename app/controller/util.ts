02304f4331a3f6a5a61402fb454b086e40c22792
cooper <1633016740@qq.com>
2024-10-14 17:21:40 +0800

    16 ing

--- app/controller/util.ts
+++ app/controller/util.ts
@@ -1,7 +1,11 @@
 import { Controller } from 'egg';
+// const nodemailer = require('nodemailer');
 export default class HomeController extends Controller {
   public async imageCode() {
     const { ctx } = this;
     ctx.body = ctx.helper.createImageCode();
+  }
+  public async emailCode() {
+    // const { ctx } = this;
   }
 }
--- app/router.ts
+++ app/router.ts
@@ -3,6 +3,7 @@ import { Application } from 'egg';
 export default (app: Application) => {
   const { controller, router } = app;
   router.get('/', controller.home.index);
-  router.get('/captcha', controller.util.imageCode);
+  router.get('/imagecode', controller.util.imageCode);
+  router.get('/emailcode', controller.util.emailCode);
   router.post('/register', controller.user.create);
 };
